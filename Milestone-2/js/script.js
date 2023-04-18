c5b3ad775cea907be3cff47fdba34c8afa68448e
Devfrank94 <francescomurro94@gmail.com>
2023-04-18 15:07:14 +0200

    partial commit

--- Milestone-2/js/script.js
+++ Milestone-2/js/script.js
@@ -2,15 +2,30 @@ import contacts from './db.js'
 
 const {createApp} = Vue;
 
+// inizializzo un oggetto globale luxon
+const dt = luxon.DateTime
+
 createApp({
 
 data(){
 return{
     contacts,
     index: 0,
-
+    clock: '',
 
     }
+},
+
+methods: {
+printClock(){
+    this.clock=dt.now().setLocale('it').toLocaleString(dt.TIME_24_SIMPLE);
+},
+
+},
+
+mounted() {
+    this.printClock()
+
 },
 
 
