7de28b7377c64b5c2397c5368840143ca8f96f6f
Devfrank94 <francescomurro94@gmail.com>
2023-04-18 15:25:18 +0200

    first commit Milestone 3

---
+++ Milestone-3/js/script.js
@@ -0,0 +1,34 @@
+import contacts from './db.js'
+
+const {createApp} = Vue;
+
+// inizializzo un oggetto globale luxon
+const dt = luxon.DateTime
+
+createApp({
+
+data(){
+return{
+    contacts,
+    index: 0,
+    clock: '',
+
+    }
+},
+
+methods: {
+
+// Funzione stampa orario attuale
+printClock(){
+    this.clock=dt.now().setLocale('it').toLocaleString(dt.TIME_24_SIMPLE);
+},
+
+},
+
+mounted() {
+    this.printClock()
+
+},
+
+
+}).mount("#app")
