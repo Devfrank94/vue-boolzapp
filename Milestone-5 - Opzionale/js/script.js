fc2843d0053edb2f168e759c5df88bfb8c74469e
Devfrank94 <francescomurro94@gmail.com>
2023-04-19 14:57:18 +0200

    first commit Milestone 5

---
+++ Milestone-5 - Opzionale/js/script.js
@@ -0,0 +1,70 @@
+import contacts from './db.js'
+
+const {createApp} = Vue;
+
+// inizializzo un oggetto globale luxon
+const dt = luxon.DateTime
+
+createApp({
+
+data(){
+return{
+    contacts,
+    index: 0,
+    clock: '',
+    inpMessage: '',
+    nameFilter: ''
+
+    }
+},
+
+methods: {
+
+// Funzione stampa orario attuale
+printClock(){
+    this.clock = dt.now().setLocale('it').toLocaleString(dt.TIME_24_SIMPLE);
+},
+
+// Funzione stampa nuovo messaggio in send
+newMess(){
+
+  const newMess = {
+    message: this.inpMessage,
+    date: dt.now().setLocale('it').toLocaleString(dt.DATETIME_SHORT_WITH_SECONDS),
+    status: 'sent'
+    }
+    this.inpMessage= ''
+    this.contacts[this.index].messages.push(newMess);
+    this.respMess();
+    },
+
+
+respMess(){
+    setTimeout(() => {
+    const message = {
+        date: dt.now().setLocale('it').toLocaleString(dt.DATETIME_SHORT_WITH_SECONDS),
+        message: 'Ok',
+        status: 'received'
+    }
+    this.contacts[this.index].messages.push(message);
+    }, 1000);
+
+},
+
+search(){
+    this.contacts.forEach(contact => {
+    contact.visible = contact.name.toLowerCase().includes(this.nameFilter.toLowerCase())
+    console.log(this.nameFilter)
+    })
+},
+
+},
+
+
+mounted() {
+    this.printClock()
+},
+
+
+
+}).mount("#app")
